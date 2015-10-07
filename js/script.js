ea046ff4d3b0f535da29c0ce15dfdbd6449b1ac2
robhogfeldt-fron15 <robhogfeldt@hotmail.com>
2015-10-07 22:07:04 +0200

    Test till egna

--- js/script.js
+++ js/script.js
@@ -10,3 +10,17 @@ $.ajax({
               });
        }
        });
+       
+       
+  $.ajax({
+       url: '../lessons/lessons.json',
+       dataType: 'json',
+       type: 'get',
+       cache: false,
+       success: function(data){
+              var html = '';
+              $(data.elever).each(function(index, value){
+                     $(".lessonList").append("<li><a href='" + value.title +"'>" + value.date + "</a></li>");
+              });
+       }
+       });
