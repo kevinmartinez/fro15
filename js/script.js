0cb93eac2aeefbfe425637355daa9383796f29f0
Kevin <kevin@pixelkojan.se>
2015-10-30 15:46:48 +0100

    merge commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//DPY
 $.ajax({
        url: 'dpy.json',
        dataType: 'json',
@@ -12,27 +13,64 @@ $.ajax({
        });
 
 
-       $.ajax({
+
+
+
+ //Lessons
+$.ajax({
        url: 'lessons.json',
        dataType: 'json',
        type: 'get',
        cache: false,
        success: function(data){
 
-              var html = '';
-              $(data.lessons).each(function(index, value){
-                  var title = value.title,
-                      lessons = value.lessons;
+       var $ul = $('<ul></ul>');
+              getList(data.lessons, $ul);
 
-                  // Lägg till kursens titel
-                  $(".lessonList").append("<h2>"+ title +"</h2>");
-                  
-                  // Loopa igenom alla lektioner för just den kursen
-                  for (var i = 0; i < lessons.length; i++){
-                    var lesson = lessons[i];
 
-                    $(".lessonList").append("<li>" + lesson.date  + " <a href='" + lesson.url +"'>" + lesson.title + "</a></li>");
-                  }
-              });
-       }
+             }
        });
+
+
+
+
+function getList(item, $list) {
+
+        if($.isArray(item)){
+            $.each(item, function (key, value) {
+                getList(value, $list);
+            });
+            return;
+        }
+
+
+
+        if (item) {
+            var $li = $('<li />')
+            .css({
+                "list-style-type" : "none"
+            });
+            if (item.bullets.length > 0) {
+                $(".lessonList").append($('<li><a href=" '+ item.url  +' ">' + item.title + '</a>' + '<button class="bpimg"></button></li>'));
+            } else {
+                $(".lessonList").append($('<li><a href=" ' + item.url + '">' + item.title + '</a></li>'));
+            }
+            if (item.bullets && item.bullets.length) {
+                var $sublist = $("<ul/>")
+                    .addClass('subList')
+
+
+                $.each(item.bullets, function(i)
+                    {
+                        $sublist.append($('<li>' + item.bullets[i] + '</li>'));
+                });
+                $li.append($sublist)
+
+            }
+            $(".lessonList").append($li)
+        }
+
+         $("button").unbind().click(function(){
+             $(this).parent().next().find('.subList').slideToggle();
+        });
+}
